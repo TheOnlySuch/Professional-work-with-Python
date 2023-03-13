88f7836a804af6a4dcb2e692db9e533ec34180a8
TheOnlySuch <118209545+TheOnlySuch@users.noreply.github.com>
2023-03-13 10:51:33 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from application.salary import calculate_salary
+from application.db.people import get_employees
+import datetime
+import requests
+if __name__ == '__main__':
+    print(datetime.date.today())
+    calculate_salary()
+    get_employees()
+
